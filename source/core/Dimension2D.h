a3fbca22f5f9c10a2104837143ff129fe4aa1a50
fedoriusv <fedoriusv@gmail.com>
2014-06-16 19:07:43 +0300

    +added inl files

--- source/core/Dimension2D.h
+++ source/core/Dimension2D.h
@@ -8,6 +8,8 @@ namespace v3d
 {
 namespace core
 {
+    /////////////////////////////////////////////////////////////////////////////////////////////////////
+
 	template <class T>
 	class TVector2D;
 
@@ -43,138 +45,15 @@ namespace core
 		T height;
 	};
 
+    /////////////////////////////////////////////////////////////////////////////////////////////////////
+
 	typedef TDimension2D<u32> Dimension2D;
 
-	template <class T>
-	TDimension2D<T>::TDimension2D()
-		: width(0)
-		, height(0)
-	{
-	}
-
-	template <class T>
-	TDimension2D<T>::TDimension2D(const T& width, const T& height)
-		: width(width)
-		, height(height)
-	{
-	}
-
-	template <class T>
-	TDimension2D<T>::TDimension2D(const TVector2D<T>& other)
-		: width(other.x)
-		, height(other.y)
-	{
-	}
-
-	template <class T>
-	TDimension2D<T>::TDimension2D(const TDimension2D<T>& other) 
-		: width(other.width)
-		, height(other.height)
-	{
-	}
-
-	template <class T>
-	TDimension2D<T>& TDimension2D<T>::operator = (const TDimension2D<T>& other)
-	{
-		width = other.width;
-		height = other.height;
-		return *this;
-	}
-
-	template <class T>
-	bool TDimension2D<T>::operator == (const TDimension2D<T>& other) const
-	{
-		const bool isEquals = core::isEquals(width, other.width) &&
-			core::isEquals(height, other.height);
-		return isEquals;
-	}
-
-	template <class T>
-	bool TDimension2D<T>::operator != (const TDimension2D<T>& other) const
-	{
-		return !(*this == other);
-	}
-
-	template <class T>
-	bool TDimension2D<T>::operator == (const TVector2D<T>& other) const
-	{
-		const bool isEquals = core::isEquals(width, other.x) &&
-			core::isEquals(height, other.y);
-		return isEquals;
-	}
-
-	template <class T>
-	bool TDimension2D<T>::operator != (const TVector2D<T>& other) const
-	{
-		return !(*this == other);
-	}
-
-	template <class T>
-	TDimension2D<T>& TDimension2D<T>::set(const T& nWidth, const T& nHeight)
-	{
-		width = nWidth;
-		height = nHeight;
-		return *this;
-	}
-
-	template <class T>
-	TDimension2D<T>& TDimension2D<T>::operator /= (const T& scale)
-	{
-		width /= scale;
-		height /= scale;
-		return *this;
-	}
-
-	template <class T>
-	TDimension2D<T> TDimension2D<T>::operator / (const T& scale) const
-	{
-		return TDimension2D<T>(width / scale, height / scale);
-	}
-
-	template <class T>
-	TDimension2D<T>& TDimension2D<T>::operator *= (const T& scale)
-	{
-		width *= scale;
-		height *= scale;
-		return *this;
-	}
-
-	template <class T>
-	TDimension2D<T> TDimension2D<T>::operator * (const T& scale) const
-	{
-		return TDimension2D<T>(width * scale, height * scale);
-	}
-
-	template <class T>
-	TDimension2D<T>& TDimension2D<T>::operator += (const TDimension2D<T>& other)
-	{
-		width += other.width;
-		height += other.height;
-		return *this;
-	}
-
-	template <class T>
-	TDimension2D<T>& TDimension2D<T>::operator -= (const TDimension2D<T>& other)
-	{
-		width -= other.Width;
-		height -= other.Height;
-		return *this;
-	}
-
-	template <class T>
-	TDimension2D<T> TDimension2D<T>::operator+(const TDimension2D<T>& other) const
-	{
-		return TDimension2D<T>(width + other.width, height + other.height);
-	}
-
-	template <class T>
-	T TDimension2D<T>::getArea() const
-	{
-		return width * height;
-	}
-
+    /////////////////////////////////////////////////////////////////////////////////////////////////////
 }
 }
+
+#include "Dimension2D.inl"
 
 #endif //_V3D_DIMENSION_2D_H_
 
