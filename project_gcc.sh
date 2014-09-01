483246890f6d9aeb4b296fa12792d794ec06f654
root <root@sneg.snegd>
2014-09-02 01:51:15 +0300

    +add sh for gcc compiler

---
+++ project_gcc.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+PROJECT_DIR=project
+CODE_DIR=linux
+
+echo "Run poject_gcc"
+
+if [  ! -d ${PROJECT_DIR} ] ; then
+    mkdir ${PROJECT_DIR}
+fi
+cd ./${PROJECT_DIR}
+
+if [  ! -d ${CODE_DIR} ] ; then
+    mkdir ${CODE_DIR}
+fi
+cd ./${CODE_DIR}
+
+cmake -DCOMPILER_GCC=ON  ../.. $@   || exit $?
+make
+
+cd ../..
+
