f6fee00deb356aaddc2cc5d723e8025b8db1f3d7
fedorius <fedoriusv@gmail.com>
2016-01-12 02:19:09 +0200

    *fix win platform caption

--- project_msvc2015.bat
+++ project_msvc2015.bat
@@ -1,5 +1,5 @@
 @echo off
-if not exist project\win32 mkdir project\win32
-cd project\win32
+if not exist project\win32 mkdir project\win
+cd project\win
 cmake -DCOMPILER_MSVC=ON -G"Visual Studio 14 Win64" ../..
 pause
