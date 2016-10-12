af380a431beccda5ecf10ca0c7e7faf92203beff
FRED\fedorius <fedoriusv@gmail.com>
2016-10-12 08:55:23 +0300

    +add swapshain for vk

---
+++ source/context/SwapChainVK.cpp
@@ -0,0 +1,328 @@
+#include "SwapChainVK.h"
+#include "utils/Logger.h"
+
+#if defined(_VULKAN_RENDER_)
+#include "DebugVK.h"
+#include "DeviceContextVK.h"
+#if defined(_PLATFORM_WIN_)
+#   include "platform/WindowWinApi.h"
+#endif //_PLATFORM_WIN_
+
+namespace v3d
+{
+namespace renderer
+{
+namespace vk
+{
+
+SwapChainVK::SwapChainVK(const ContextPtr context)
+    : m_currentBuffer(0)
+    , m_swapChain(VK_NULL_HANDLE)
+    , m_surface(VK_NULL_HANDLE)
+    , m_queueFamilyIndex(0)
+    , m_queue(VK_NULL_HANDLE)
+
+    , m_instance(VK_NULL_HANDLE)
+    , m_device(VK_NULL_HANDLE)
+    , m_physicalDevice(VK_NULL_HANDLE)
+#if defined(_PLATFORM_WIN_)
+    , m_appInstance(NULL)
+    , m_appWindow(NULL)
+#endif //_PLATFORM_WIN_
+{
+#if defined(_PLATFORM_WIN_)
+    m_appInstance = GetModuleHandle(NULL);
+    m_appWindow = std::static_pointer_cast<const platform::WindowWinApi>(context->getWindow())->getHandleWindow();
+#endif //_PLATFORM_WIN_
+    m_instance = std::static_pointer_cast<const DeviceContextVK>(context)->getVulkanInstance();
+    m_physicalDevice = std::static_pointer_cast<const DeviceContextVK>(context)->getVulkanPhysicalDevice();
+
+    m_surfaceSize.width = context->getWindowSize().width;
+    m_surfaceSize.height = context->getWindowSize().height;
+    m_isVSync = context->isVSync();
+}
+
+SwapChainVK::~SwapChainVK()
+{
+    ASSERT(!m_swapChain, "m_swapChain is exist");
+    ASSERT(!m_surface, "m_surface is exsit");
+    ASSERT(!m_queue, "m_queue is exist");
+}
+
+bool SwapChainVK::create()
+{
+    if (SwapChainVK::createSurface())
+    {
+        LOG_ERROR(" SwapChainVK::create: cannot create surface");
+        SwapChainVK::destroy();
+
+        return false;
+    }
+
+    if (SwapChainVK::createSwapChain())
+    {
+        LOG_ERROR(" SwapChainVK::create: cannot create swapchain");
+        SwapChainVK::destroy();
+
+        return false;
+    }
+
+    return true;
+}
+
+void SwapChainVK::destroy()
+{
+    if (m_swapChain != VK_NULL_HANDLE)
+    {
+        vkDestroySwapchainKHR(m_device, m_swapChain, nullptr);
+        m_swapChain = VK_NULL_HANDLE;
+    }
+
+    if (m_surface != VK_NULL_HANDLE)
+    {
+        vkDestroySurfaceKHR(m_instance, m_surface, nullptr);
+        m_surface = VK_NULL_HANDLE;
+    }
+}
+
+bool SwapChainVK::update(const core::Dimension2D& size, bool vsync)
+{
+    if (m_swapChain != VK_NULL_HANDLE)
+    {
+        SwapChainVK::destroy();
+    }
+
+    m_surfaceSize.width = size.width;
+    m_surfaceSize.height = size.height;
+    m_isVSync = vsync;
+
+    return SwapChainVK::create();
+}
+
+void SwapChainVK::presentFrame()
+{
+    VkPresentInfoKHR presentInfo;
+    presentInfo.sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR;
+    presentInfo.pNext = nullptr;
+    presentInfo.swapchainCount = 1;
+    presentInfo.pSwapchains = &m_swapChain;
+    presentInfo.pImageIndices = &m_currentBuffer;
+    presentInfo.pWaitSemaphores = &semaphore;
+    presentInfo.waitSemaphoreCount = 1;
+    presentInfo.pResults = nullptr;
+
+    VkResult result = vkQueuePresentKHR(m_queue, &presentInfo);
+    if (result == VK_ERROR_OUT_OF_DATE_KHR)
+    {
+        LOG_WARNING("SwapChainVK::presentFrame: Error VK_ERROR_OUT_OF_DATE_KHR. Swapchain is out of date. Needs to be recreated for defined results");
+    }
+    else if (result != VK_SUCCESS)
+    {
+        LOG_ERROR("SwapChainVK::presentFrame: vkQueuePresentKHR. Error %s", DebugVK::errorString(result).c_str());
+    }
+}
+
+s32 SwapChainVK::prepareFrame()
+{
+    u32 outImageIndex = 0;
+    VkResult result = vkAcquireNextImageKHR(m_device, m_swapChain, UINT64_MAX, semaphore, VK_NULL_HANDLE, &outImageIndex);
+    if (result == VK_ERROR_OUT_OF_DATE_KHR)
+    {
+        LOG_WARNING("SwapChainVK::prepareFrame: Error VK_ERROR_OUT_OF_DATE_KHR. Swapchain is out of date. Needs to be recreated for defined results");
+    }
+    else if (result != VK_SUCCESS)
+    {
+        LOG_ERROR("SwapChainVK::prepareFrame: vkAcquireNextImageKHR. Error %s", DebugVK::errorString(result).c_str());
+        return m_currentBuffer;
+    }
+    m_currentBuffer = outImageIndex;
+
+    return m_currentBuffer;
+}
+
+bool SwapChainVK::createSurface()
+{
+    LOG_DEBUG("SwapChainVK::createSurface");
+#if defined(_PLATFORM_WIN_)
+    if (!SwapChainVK::createSurfaceWinApi())
+    {
+        LOG_ERROR("SwapChainVK::createSurface: Create win surface is falied");
+        return false;
+    }
+#else //_PLATFORM_WIN_
+    LOG_ERROR("SwapChainVK::createSurface: paltform not supported");
+    return false;
+#endif //_PLATFORM_WIN_
+
+    VkResult result = vkGetPhysicalDeviceSurfaceCapabilitiesKHR(m_physicalDevice, m_surface, &m_surfaceCaps);
+    if (result != VK_SUCCESS)
+    {
+        LOG_ERROR("SwapChainVK::createSurface: vkGetPhysicalDeviceSurfaceCapabilitiesKHR. Error %s", DebugVK::errorString(result).c_str());
+        return false;
+    }
+
+    if (m_surfaceCaps.maxImageCount < 2)
+    {
+        LOG_ERROR("SwapChainVK::createSurface: Not enough images supported in vulkan swapchain");
+        return false;
+    }
+
+    VkBool32 supportsPresentation = false;
+    vkGetPhysicalDeviceSurfaceSupportKHR(m_physicalDevice, m_queueFamilyIndex, m_surface, &supportsPresentation);
+    if (!supportsPresentation)
+    {
+        LOG_ERROR("SwapChainVK::createSurface: not support presentation");
+        return false;
+    }
+
+    //Get Surface format
+    u32 surfaceFormatCount;
+    result = vkGetPhysicalDeviceSurfaceFormatsKHR(m_physicalDevice, m_surface, &surfaceFormatCount, nullptr);
+    if (result != VK_SUCCESS)
+    {
+        LOG_ERROR("SwapChainVK::createSurface: vkGetPhysicalDeviceSurfaceFormatsKHR. Error %s", DebugVK::errorString(result).c_str());
+        return false;
+    }
+
+    std::vector<VkSurfaceFormatKHR> surfaceFormats(surfaceFormatCount);
+    result = vkGetPhysicalDeviceSurfaceFormatsKHR(m_physicalDevice, m_surface, &surfaceFormatCount, surfaceFormats.data());
+    if (result != VK_SUCCESS)
+    {
+        LOG_ERROR("SwapChainVK::createSurface: vkGetPhysicalDeviceSurfaceFormatsKHR. Error %s", DebugVK::errorString(result).c_str());
+        return false;
+    }
+    std::for_each(surfaceFormats.begin(), surfaceFormats.end(), [](const VkSurfaceFormatKHR& surfaceFormat)
+    {
+        LOG_INFO("SurfaceFormat supported format: %d, colorspace: %d", surfaceFormat.format, surfaceFormat.colorSpace);
+    });
+
+    u32 surfaceFormatIndex = 0;
+    // If the surface format list only includes one entry with VK_FORMAT_UNDEFINED,
+    // there is no preferered format, so we assume VK_FORMAT_B8G8R8A8_UNORM
+    if ((surfaceFormatCount == 1) && (surfaceFormats[0].format == VK_FORMAT_UNDEFINED))
+    {
+        surfaceFormatCount = VK_FORMAT_B8G8R8A8_UNORM;
+    }
+    else
+    {
+        // Always select the first available color format
+        // If you need a specific format (e.g. SRGB) you'd need to
+        // iterate over the list of available surface format and
+        // check for it's presence
+        m_surfaceFormat.format = surfaceFormats[0].format;
+    }
+    m_surfaceFormat.colorSpace = surfaceFormats[0].colorSpace;
+
+    return true;
+}
+
+
+bool SwapChainVK::createSwapChain()
+{
+    LOG_DEBUG("SwapChainVK::createSwapChain");
+
+    ASSERT(m_surface, "surface is nullptr");
+
+    // Select a present mode for the swapchain
+    u32 presentModeCount = 0;
+    vkGetPhysicalDeviceSurfacePresentModesKHR(m_physicalDevice, m_surface, &presentModeCount, nullptr);
+
+    std::vector<VkPresentModeKHR> presentModes(presentModeCount);
+    vkGetPhysicalDeviceSurfacePresentModesKHR(m_physicalDevice, m_surface, &presentModeCount, presentModes.data());
+
+    // The VK_PRESENT_MODE_FIFO_KHR mode must always be present as per spec
+    // This mode waits for the vertical blank ("v-sync")
+    VkPresentModeKHR swapchainPresentMode = VK_PRESENT_MODE_FIFO_KHR;
+
+    // If v-sync is not requested, try to find a mailbox mode
+    // It's the lowest latency non-tearing present mode available
+    if (!m_isVSync)
+    {
+        for (u32 i = 0; i < presentModeCount; i++)
+        {
+            if (presentModes[i] == VK_PRESENT_MODE_MAILBOX_KHR)
+            {
+                swapchainPresentMode = VK_PRESENT_MODE_MAILBOX_KHR;
+                break;
+            }
+
+            if ((swapchainPresentMode != VK_PRESENT_MODE_MAILBOX_KHR) && (presentModes[i] == VK_PRESENT_MODE_IMMEDIATE_KHR))
+            {
+                swapchainPresentMode = VK_PRESENT_MODE_IMMEDIATE_KHR;
+            }
+        }
+    }
+
+    // Determine the number of images
+    uint32_t desiredNumberOfSwapchainImages = m_surfaceCaps.minImageCount + 1;
+    if ((m_surfaceCaps.maxImageCount > 0) && (desiredNumberOfSwapchainImages > m_surfaceCaps.maxImageCount))
+    {
+        desiredNumberOfSwapchainImages = m_surfaceCaps.maxImageCount;
+    }
+
+    // Find the transformation of the surface
+    VkSurfaceTransformFlagsKHR preTransform;
+    if (m_surfaceCaps.supportedTransforms & VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR)
+    {
+        // We prefer a non-rotated transform
+        preTransform = VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR;
+    }
+    else
+    {
+        preTransform = m_surfaceCaps.currentTransform;
+    }
+
+    VkSwapchainCreateInfoKHR swapChainInfo = {};
+    swapChainInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+    swapChainInfo.pNext = nullptr;
+    swapChainInfo.surface = m_surface;
+    swapChainInfo.minImageCount = desiredNumberOfSwapchainImages;
+    swapChainInfo.imageFormat = m_surfaceFormat.format;
+    swapChainInfo.imageColorSpace = m_surfaceFormat.colorSpace;
+    swapChainInfo.imageExtent = { m_surfaceSize.width, m_surfaceSize.height };
+    swapChainInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+    swapChainInfo.preTransform = (VkSurfaceTransformFlagBitsKHR)preTransform;
+    swapChainInfo.imageArrayLayers = 1;
+    swapChainInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+    swapChainInfo.queueFamilyIndexCount = 0;
+    swapChainInfo.pQueueFamilyIndices = nullptr;
+    swapChainInfo.presentMode = swapchainPresentMode;
+    swapChainInfo.oldSwapchain = nullptr;
+    swapChainInfo.clipped = VK_TRUE; // Setting clipped to VK_TRUE allows the implementation to discard rendering outside of the surface area
+    swapChainInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+
+    VkResult result = vkCreateSwapchainKHR(m_device, &swapChainInfo, nullptr, &m_swapChain);
+    if (result != VK_SUCCESS)
+    {
+        LOG_ERROR("SwapChainVK::createSurface: vkGetPhysicalDeviceSurfaceFormatsKHR. Error %s", DebugVK::errorString(result).c_str());
+        return false;
+    }
+
+    return true;
+}
+
+#if defined(_PLATFORM_WIN_)
+bool SwapChainVK::createSurfaceWinApi()
+{
+    VkWin32SurfaceCreateInfoKHR surfaceCreateInfo = {};
+    surfaceCreateInfo.sType = VK_STRUCTURE_TYPE_WIN32_SURFACE_CREATE_INFO_KHR;
+    surfaceCreateInfo.pNext = nullptr;
+    surfaceCreateInfo.flags = 0;
+    surfaceCreateInfo.hinstance = m_appInstance;
+    surfaceCreateInfo.hwnd = m_appWindow;
+    VkResult result = vkCreateWin32SurfaceKHR(m_instance, &surfaceCreateInfo, nullptr, &m_surface);
+    if (result != VK_SUCCESS)
+    {
+        LOG_ERROR("SwapChainVK::createSurfaceWinApi: vkCreateWin32SurfaceKHR. Error %s", DebugVK::errorString(result).c_str());
+        return false;
+    }
+
+    return true;
+}
+#endif //_PLATFORM_WIN_
+
+} //namespace vk
+} //namespace renderer
+} //namespace v3d
+
+#endif //_VULKAN_RENDER_
---
+++ source/context/SwapChainVK.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include "common.h"
+#include "DeviceContext.h"
+
+#if defined(_VULKAN_RENDER_)
+#   include <vulkan/vulkan.h>
+
+#   if defined(_PLATFORM_WIN_)
+#   include <windows.h>
+#   endif //_PLATFORM_WIN_
+
+namespace v3d
+{
+namespace renderer
+{
+namespace vk
+{
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    class SwapChainVK
+    {
+    public:
+
+        SwapChainVK(const ContextPtr context);
+        ~SwapChainVK();
+
+        bool                        create();
+        void                        destroy();
+
+        bool                        update(const core::Dimension2D& size, bool vsync);
+
+        void                        presentFrame();
+        s32                         prepareFrame();
+
+    private:
+
+        bool                        createSurface();
+        bool                        createSwapChain();
+
+        u32                         m_currentBuffer;
+
+        VkSwapchainKHR              m_swapChain;
+        VkSurfaceKHR                m_surface;
+
+        s32                         m_queueFamilyIndex;
+        VkQueue                     m_queue; //?
+
+        VkInstance                  m_instance;
+        VkDevice                    m_device;
+        VkPhysicalDevice            m_physicalDevice;
+
+        VkSurfaceCapabilitiesKHR    m_surfaceCaps;
+
+        VkSurfaceFormatKHR          m_surfaceFormat;
+        VkExtent2D                  m_surfaceSize;
+        VkBool32                    m_isVSync;
+
+#if defined(_PLATFORM_WIN_)
+        bool                        createSurfaceWinApi();
+
+        HINSTANCE                   m_appInstance;
+        HWND                        m_appWindow;
+#endif //_PLATFORM_WIN_
+    };
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+} //namespace vk
+} //namespace renderer
+} //namespace v3d
+
+#endif //_VULKAN_RENDER_
